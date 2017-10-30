710ce03c3652a3f9f8a00d13d4b41e31bb02e7bb
“hulin <hulin@qiyi.com>
2017-10-30 17:15:39 +0800

    更新代码

--- dubbo-api/src/main/java/com/hulin/demo/DemoService.java
+++ dubbo-api/src/main/java/com/hulin/demo/DemoService.java
@@ -4,5 +4,5 @@ package com.hulin.demo;
  * Created by hulin on 2017/10/30.
  */
 public interface DemoService {
-    void sayHello(String name);
+    String sayHello(String name);
 }
--- dubbo-provider/src/main/java/com/hulin/demo/provider/DemoServiceImpl.java
+++ dubbo-provider/src/main/java/com/hulin/demo/provider/DemoServiceImpl.java
@@ -6,7 +6,7 @@ import com.hulin.demo.DemoService;
  * Created by hulin on 2017/10/30.
  */
 public class DemoServiceImpl implements DemoService {
-    public void sayHello(String name) {
-        System.out.println(name + ": say Hello!");
+    public String sayHello(String name) {
+        return name + ": say Hello!";
     }
 }
